8e17aef2085f8ae35900822a60637140da9896fd
Sarcasl <venwes@gmail.com>
2023-04-27 17:10:03 -0700

    Update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//Creating a customer survey 
+//Creating a customer survey using key pairs
 
 
-//Object with key pairs
 
+//Object (customer) with key pairs
 const customer = {
     firstName: "jake",
     lastName: "smith",
     email: "jaekSmih!aol.com",
     phone: undefined,
     zipCode: "631",
-    favoriteFlavors: 32,
+    favoriteFlavors: "32",
     cupSize: "medium",
     favoriteStore: "Target",
     firstVisit: false
@@ -17,44 +17,25 @@ const customer = {
 
 
 
-
-
-
-
 //Object updates
-
 customer ["email"] = "Jak3Smith1992@email.com"
-customer ["phone"] = 3195551234
+customer ["phone"] = "3195551234"
 customer ["zipCode"] = "63132"
 customer ["favoriteFlavors"] = ["coffee", "strawberry", "matcha"]
 
 
 
 
-
-
-
-
 //Object deletions
-
 delete customer ["zipCode"]
 delete customer ["favoriteStore"]
 
 
 
-
-
-
-
 //Object additions
-
 customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"]
 customer.futureFlavors = "mango"
 customer.todaysPurchaseCost = "5.32"
-
-
-
-
 
 
 
